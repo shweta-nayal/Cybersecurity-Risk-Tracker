5bceaaa1ba9130075a897ef92705656fd56c499c
Shweta <88674590+shweta-nayal@users.noreply.github.com>
2026-02-26 19:48:15 +0530

    added home page

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from risks.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
